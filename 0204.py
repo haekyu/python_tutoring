4550ef8c3f6170ddfca57be809a9da20b286b975
haekyu <hkpark627@gmail.com>
2018-02-04 15:59:46 +0900

    0204

---
+++ 0204.py
@@ -0,0 +1,21 @@
+리스트를 만드는데  ['berry', 'is', 'lovely'] 만들고
+거기다가 [1, 2, 3] 를 concatenate 하고 확인해보자
+
+print(lst)
+
+
+lst = ['berry', 'is', 'lovely']
+lst + [1, 2, 3]
+
+lst = [0, 'a', 3, [2, 3, 1], 3]
+lst.append('Hi')
+
+print(lst)
+
+'''
+s = "abcde"
+
+# "e"
+print(s[-1])
+print(s[len(s) - 1])
+'''
